a913cd912b6a2c837ccec324b77b4afaa2efd7d1
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-18 16:25:32 -0400

    Initial Commit

---
+++ fo-rest/src/main/java/com/forest/forest/FoRestApplication.java
@@ -0,0 +1,13 @@
+package com.forest.forest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoRestApplication.class, args);
+	}
+
+}
