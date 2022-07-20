95a5b896ff4225c9da14fb61fadea0c85da571b6
tlb99 <28433525+tlb99@users.noreply.github.com>
2022-07-19 17:18:57 -0700

    added some user functionality; still need authentication

--- fo-rest/src/main/java/com/forest/forest/controllers/UserController.java
+++ fo-rest/src/main/java/com/forest/forest/controllers/UserController.java
@@ -1,5 +1,54 @@
 package com.forest.forest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.forest.forest.dto.LoginDto;
+import com.forest.forest.models.User;
+import com.forest.forest.repository.UserRepository;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+@RestController
+@Validated
 public class UserController {
-    
+
+    @Autowired
+    private UserRepository repository;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    /* 
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@Valid @RequestBody LoginDto loginDto){
+		
+            Authentication authObject = null;
+            try {
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+                SecurityContextHolder.getContext().setAuthentication(authObject);
+            } catch (BadCredentialsException e) {
+                return new ResponseEntity<>("Credentials are invalid.", HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>("User logged in successfully!", HttpStatus.OK);
+    }*/
+
 }
