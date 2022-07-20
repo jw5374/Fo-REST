699023aedde7f967633cdce5ba39e515f7fe8dc8
VenskyCincighetti <steven.ray.coronel@gmail.com>
2022-07-20 07:56:20 -0700

    Created User.java, Cart.java, Product.java (didn't realize Anthony was making User.java, used his pushed user.java as a reference for mine). Created ProductRepository.java and CartRepository.java (needs suggestions for productRespository, haven't done much for cartRepository yet though. Needs to know what joseph needs for his website too.)

--- fo-rest/src/main/java/com/forest/forest/repository/CartRepository.java
+++ fo-rest/src/main/java/com/forest/forest/repository/CartRepository.java
@@ -8,11 +8,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.forest.forest.models.Cart;
 import com.forest.forest.models.Product;
 import com.forest.forest.models.User;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long>
 {
 	//Does this work??
--- fo-rest/src/main/java/com/forest/forest/repository/ProductRepository.java
+++ fo-rest/src/main/java/com/forest/forest/repository/ProductRepository.java
@@ -7,9 +7,11 @@ import java.util.Optional;
 import javax.persistence.Column;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.forest.forest.models.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> 
 {
 	
