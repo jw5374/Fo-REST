ad3b059e6e4e1a98ff97954f320a06b805edf215
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-22 17:09:53 -0400

    resetting all security config

--- fo-rest/src/main/java/com/forest/forest/config/SecurityConfig.java
+++ fo-rest/src/main/java/com/forest/forest/config/SecurityConfig.java
@@ -1,25 +1,26 @@
 package com.forest.forest.config;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+// import java.io.IOException;
+// import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
+// import javax.servlet.ServletException;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.core.AuthenticationException;
+// import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+// import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+// import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,59 +28,61 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class SecurityConfig {
 
-  @Autowired
-  private DataSource dataSource;
+  // @Autowired
+  // private DataSource dataSource;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-        .cors().and()
-        .csrf().disable()
-        .authorizeRequests()
-          .antMatchers("/", "/home", "/register**", "/products/**", "/products**").permitAll() 
-          .anyRequest().authenticated() 
-          .and()
-       .formLogin()
-       .successHandler(new AuthenticationSuccessHandler() {
-          @Override
-          public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-            response.setStatus(200);
-            response.setContentType("application/json");
-            PrintWriter res = response.getWriter();
-            res.println("{ \"username\": " + "\"" + request.getParameter("username") + "\" }" );
-          }
-        })
-        .failureHandler(new AuthenticationFailureHandler() {
-          @Override
-          public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-              AuthenticationException exception) throws IOException, ServletException {
-                response.setStatus(400);
-                response.setContentType("application/json");
-                PrintWriter res = response.getWriter();
-                res.println("{ \"username\": " + "\"" + request.getParameter("username") + "\", \"password\": " + "\"" + request.getParameter("password") + "\" }" );
-          }
-        })
-        .loginProcessingUrl("/login")
-         .permitAll()
-         .and()
-      .logout()
-        .logoutUrl("/logout") 
-        .permitAll()
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID");
+        http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+      //   .cors().and()
+      //   .csrf().disable()
+      //   .authorizeRequests()
+      //     .antMatchers("/", "/home", "/register**", "/products/**", "/products**").permitAll() 
+      //     .anyRequest().authenticated() 
+      //     .and()
+      //  .formLogin()
+      //  .successHandler(new AuthenticationSuccessHandler() {
+      //     @Override
+      //     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+      //       response.setStatus(200);
+      //       response.setContentType("application/json");
+      //       response.setHeader("Access-Control-Allow-Origin", "null");
+      //       response.setHeader("Access-Control-Allow-Credentials", "true");
+      //       PrintWriter res = response.getWriter();
+      //       res.println("{ \"username\": " + "\"" + request.getParameter("username") + "\" }" );
+      //     }
+      //   })
+      //   .failureHandler(new AuthenticationFailureHandler() {
+      //     @Override
+      //     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+      //         AuthenticationException exception) throws IOException, ServletException {
+      //           response.setStatus(400);
+      //           response.setContentType("application/json");
+      //           PrintWriter res = response.getWriter();
+      //           res.println("{ \"username\": " + "\"" + request.getParameter("username") + "\", \"password\": " + "\"" + request.getParameter("password") + "\" }" );
+      //     }
+      //   })
+      //   .loginProcessingUrl("/login")
+      //    .permitAll()
+      //    .and()
+      // .logout()
+      //   .logoutUrl("/logout") 
+      //   .permitAll()
+      //   .invalidateHttpSession(true)
+      //   .deleteCookies("JSESSIONID");
 
 
       return http.build();
   }
 
-  @Bean
-	public JdbcDaoImpl userDetailsService(){
-    JdbcDaoImpl jdbcDaoImpl = new JdbcDaoImpl();
-    jdbcDaoImpl.setDataSource(dataSource);
-    jdbcDaoImpl.setUsersByUsernameQuery("SELECT username, pass, 'true' FROM users WHERE username=?");
-    jdbcDaoImpl.setAuthoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username=?");
-    return jdbcDaoImpl;
-	}
+  // @Bean
+	// public JdbcDaoImpl userDetailsService(){
+  //   JdbcDaoImpl jdbcDaoImpl = new JdbcDaoImpl();
+  //   jdbcDaoImpl.setDataSource(dataSource);
+  //   jdbcDaoImpl.setUsersByUsernameQuery("SELECT username, pass, 'true' FROM users WHERE username=?");
+  //   jdbcDaoImpl.setAuthoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username=?");
+  //   return jdbcDaoImpl;
+	// }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
